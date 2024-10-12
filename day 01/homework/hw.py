3205babac5634b9d3856aa7ab9b534d54a417535
avto2 <zviadadzeavto359@gmail.com>
2024-10-13 00:43:33 +0400

    first commit

---
+++ day 01/homework/hw.py
@@ -0,0 +1,5 @@
+name = "avto"
+surname = "zviadadze"
+age = 12
+heigth = 150
+print("hello my name is " + name + " my surname is " + surname + "i am " + str(age)  + " years old " + "my heigth is " + str(heigth))

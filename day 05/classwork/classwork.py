8bc3af27b5995ddf7dc3d44601749af5b3ee080d
avto2 <zviadadzeavto359@gmail.com>
2024-11-09 19:03:12 +0400

    second commit

---
+++ day 05/classwork/classwork.py
@@ -0,0 +1,46 @@
+# 1
+greeting = "Hello"
+username = "student"
+age = 20
+location = "New York"
+
+#2
+print('welcome, ' + username + "!" ) 
+print(greeting + ", " + username + "!") 
+
+#3
+print("Your age is" + age)  
+print("In 5 years, you will be" + age + "5" + "years old") 
+
+#4
+favoriteNumber = 7
+doubled_number=favoriteNumber * 2
+print("Your favorite number doubled is" + favoriteNumber * 2)  
+
+#5
+total_score = 100
+bonus_points = 10
+finalScore = total_score + bonus_points
+print("Your final score is" + finalScore)
+
+#6
+days_in_a_week = 7
+print("There are" + days_in_a_week * 4 + "days in four weeks")
+
+#7
+firstName = "John"
+lastName = "smith"
+print("Hello," + firstName +  + lastName + "!")
+
+#10
+year = 2024
+print("Next year is" + year + 1)
+print("Your age next year will be" + age + 1)
+
+#11
+farewell = "Goodbye"
+print(farewell, username + "!See you next time.")
+
+#8
+nickname = "ace"
+print("Your nickname is" + nickname)

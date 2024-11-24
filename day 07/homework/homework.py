3418395dcc8529039bd6f08bdc63a860522aefcb
avto2 <zviadadzeavto359@gmail.com>
2024-11-24 18:36:36 +0400

    second commit

---
+++ day 07/homework/homework.py
@@ -0,0 +1,23 @@
+name=input("ჩაწერე შენი სახელი")
+surname=input("ჩაწერე შენი გვარი")
+print(name, surname)
+
+
+print("C A L C U L A T O R")
+
+num1=int(input("შიყვანე პირველი რიცხვი"))
+num2=int(input("შეიყვანე მეორე რიცხვი"))
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
+
+
+email=input("შეიყვანე შენი ემაილი")
+password=input("შეიყვანე შენი პაროლი")
+print(email,password)
+
+
+#პირველ კოდში მიწერია სახელის და გვარის შყვანა ინფუთით
+#მეორე კოდში მიწერიაკალკულატორი ორ რიცხვით ესენია +,-,*,/
+#მესამე კოდში მიწერია იმეილის და პაროლის შეყვანა ინფუთით

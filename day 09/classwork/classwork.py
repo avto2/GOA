9734bfc57cd7496342f58e6b7c69e108bde56e6a
avto2 <zviadadzeavto359@gmail.com>
2024-12-12 17:13:11 +0400

    second commit

---
+++ day 09/classwork/classwork.py
@@ -0,0 +1,8 @@
+print("calculator")
+num1 = float(input ("enter first number"))
+num2 = float(input ("enter second number"))
+print(num1+num2)
+print(num1-num2)
+print(num1*num2)
+print(num1/num2)
+

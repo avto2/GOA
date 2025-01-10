3028785f539c3dfcbafc9d91d9c2f215c347983b
avto2 <zviadadzeavto359@gmail.com>
2025-01-10 20:31:01 +0400

    second commit

---
+++ day 09/homework/homework.py
@@ -0,0 +1,7 @@
+#int() aris ricxvebi
+#float() aris atwiladebi
+#type() aris
+# konkatenacia aris ori an meti sityvis(stringis ) gaerTianeba
+
+username -float(input("enter ur username"))
+print("hello username")
---
+++ day 11/homework/homework.py
@@ -0,0 +1,109 @@
+
+print(5 > 8)
+print(6 > 9)
+print(9 > 9)
+print(6 > 2)
+print(6 > 7)
+print(7 > 3)
+print(2 > 7)
+print(4 > 6)
+print(9 > 4)
+print(6 > 3)
+print(1 > 3)
+print(9 > 2)
+print(8 > 6)
+print(5 > 4)
+print(4 > 9)
+
+print(5 < 5)
+print(6 < 2)
+print(7 < 4)
+print(9 < 1)
+print(5 < 3)
+print(3 < 1)
+print(6 < 7)
+print(4 < 6)
+print(9 < 4)
+print(6 < 7)
+print(1 < 3)
+print(7 < 8)
+print(8 < 6)
+print(2 < 9)
+print(4 < 9)
+
+
+
+print(5 == 5)
+print(6 == 2)
+print(7 == 4)
+print(9 == 1)
+print(5 == 3)
+print(3 == 1)
+print(6 == 7)
+print(4 == 6)
+print(9 == 4)
+print(6 == 7)
+print(1 == 3)
+print(7 == 8)
+print(8 == 6)
+print(2 == 9)
+print(4 == 9)
+
+
+
+print(5 != 5)
+print(6 != 2)
+print(7 != 4)
+print(9 != 1)
+print(5 != 3)
+print(3 != 1)
+print(6 != 7)
+print(4 != 6)
+print(9 != 4)
+print(6 != 7)
+print(1 != 3)
+print(7 != 8)
+print(8 != 6)
+print(2 != 9)
+print(4 != 9)
+
+
+x=10
+a=2
+print(a==x)
+
+x=12
+a=11
+print(a==x)
+
+x=3
+a=2
+print(a==x)
+
+x=3
+a=7
+print(a==x)
+
+x=9
+a-10
+print(a==x)
+
+x=4
+a=3
+print(a==x)
+
+x=19
+a=18
+print(a==x)
+
+x=56
+a=55
+print(a==x)
+
+x=100
+a=200
+print(a==x)
+
+x=250
+a=300
+print(a==x)

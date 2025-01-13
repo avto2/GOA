a7cc278116a1d7851acc72aa401807b34fae7156
avto2 <zviadadzeavto359@gmail.com>
2025-01-13 21:58:28 +0400

    commit

---
+++ day 14/classwork/classwork.py
@@ -0,0 +1,15 @@
+num1 = int(input("enter your age: "))
+if num1 >= 18:
+    print("you are allowed")
+else:
+    print("you are not allowed")
+print(num1)
+
+( "value" > 100) or "vip"
+
+("temperature > 30 ") or ("temperature < 30 ")
+
+print(" you can go to school if you have teachers permission")
+
+
+
---
+++ day 14/homework/homework.py
@@ -0,0 +1,9 @@
+num = int(input("გთხოვთ, შეიყვანეთ რიცხვი: "))
+
+if 5 <= num <= 15 and num != 10:
+    print("you are right")
+else:
+    print("you are not right") 
+
+name = input("enter your name")
+

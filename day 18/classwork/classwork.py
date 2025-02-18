4b3ca941d997111333f7944355df40156b796cb2
avto2 <zviadadzeavto359@gmail.com>
2025-02-18 15:45:07 +0400

    second commit

---
+++ day 18/classwork/classwork.py
@@ -0,0 +1,13 @@
+for i in range(20,41):
+    print(i)
+
+
+     name = input("please enter your name")
+
+       for i in range(15):
+        print(name)
+ 
+
+
+ for i in range(2,21,2):
+    print(i)
---
+++ day 18/homework/homework.py
@@ -0,0 +1,4 @@
+for i in range ('goa best',100):
+    print(i)  
+for i in range (20,101,10):  
+    print(i)
